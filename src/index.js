0bc6218dc77124f14474866abf586dc52396b2e0
Evan <evan.buschor@gmail.com>
2020-02-01 22:23:29 -0500

    change square to a "dumb" component

--- src/index.js
+++ src/index.js
@@ -5,11 +5,7 @@ import "./index.css";
 class Square extends React.Component {
 	render() {
 		return (
-			<button
-				className="square"
-				onClick={() => {
-					alert("clicked");
-				}}>
+			<button className="square" onClick={() => this.props.onClick()}>
 				{this.props.value}
 			</button>
 		);
@@ -17,8 +13,26 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			squares: Array(9).fill(null)
+		};
+	}
+
+	handleClick(i) {
+		const squares = this.state.squares.slice();
+		squares[i] = "X";
+		this.setState({ squares: squares });
+	}
+
 	renderSquare(i) {
-		return <Square value={i} />;
+		return (
+			<Square
+				value={this.state.squares[i]}
+				onClick={() => this.handleClick(i)}
+			/>
+		);
 	}
 
 	render() {
